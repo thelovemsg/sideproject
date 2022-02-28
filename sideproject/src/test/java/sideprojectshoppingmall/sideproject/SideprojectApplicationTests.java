e30dfaccf39e9f515a41430818ed9518668edb00
thelovemsg <thelovemsg@naver.com>
2022-02-28 12:44:04 +0900

    upload sideproject first

---
+++ sideproject/src/test/java/sideprojectshoppingmall/sideproject/SideprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package sideprojectshoppingmall.sideproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
