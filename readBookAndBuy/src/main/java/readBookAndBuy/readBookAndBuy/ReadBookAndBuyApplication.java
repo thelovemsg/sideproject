3e46d4ff52e021fe220c24419440702b9a80fefc
thelovemsg <thelovemsg@naver.com>
2022-03-04 12:11:43 +0900

    refresh project

---
+++ readBookAndBuy/src/main/java/readBookAndBuy/readBookAndBuy/ReadBookAndBuyApplication.java
@@ -0,0 +1,13 @@
+package readBookAndBuy.readBookAndBuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadBookAndBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadBookAndBuyApplication.class, args);
+	}
+
+}
---
+++ readBookAndBuy/src/test/java/readBookAndBuy/readBookAndBuy/ReadBookAndBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package readBookAndBuy.readBookAndBuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadBookAndBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
